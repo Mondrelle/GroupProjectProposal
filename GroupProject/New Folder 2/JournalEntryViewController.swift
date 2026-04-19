7b69011d24957aa0c27c297cbfcd63b032ff31a7
Mondrelle <mario_cheek10@mymail.eku.edu>
2026-04-19 16:00:56 -0400

    Improved the launchscreen design. The home screen and mood tracker ui is auto formated. Other than the segues and taps, the project is looking interesting

---
+++ GroupProject/New Folder 2/JournalEntryViewController.swift
@@ -0,0 +1,29 @@
+//
+//  JournalEntryViewController.swift
+//  GroupProject
+//
+//  Created by Mondrelle Cheek on 4/19/26.
+//
+
+import UIKit
+
+class JournalEntryViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
