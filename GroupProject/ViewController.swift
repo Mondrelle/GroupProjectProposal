adcb756d8c318567da52ac6e81e3f42a2483680b
Mondrelle <mario_cheek10@mymail.eku.edu>
2026-04-19 13:30:20 -0400

    registered taps, inclueded assests and ui from previous project proposal

--- GroupProject/ViewController.swift
+++ GroupProject/ViewController.swift
@@ -1,19 +1,34 @@
-//
 //  ViewController.swift
 //  GroupProject
-//
 //  Created by Mondrelle Cheek on 4/19/26.
-//
-
 import UIKit
 
+//Class ViewController
 class ViewController: UIViewController {
 
+    //taps
+    @IBAction func moodTapped(_ sender: Any) {
+        performSegue(withIdentifier: "goToMoodTracker", sender: self)
+    }
+    @IBAction func journalTapped(_ sender: Any) {
+        performSegue(withIdentifier: "goToJournal", sender: self)
+    }
+    
+    @IBAction func healthTapped(_ sender: Any) {
+        performSegue(withIdentifier: "goToHealthAdvisor", sender: self)
+    }
+    
+    @IBAction func profileTapped(_ sender: Any) {
+        performSegue(withIdentifier: "goToProfile", sender: self)
+    }
+    
+    
+    //Override
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
 
-}
+}//end of ViewController Class
 
