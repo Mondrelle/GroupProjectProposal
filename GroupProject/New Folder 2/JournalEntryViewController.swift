9af9205563cfd74f87ce98cbcff516e1744beb30
Mondrelle <mario_cheek10@mymail.eku.edu>
2026-04-19 16:01:40 -0400

    changes

--- GroupProject/New Folder 2/JournalEntryViewController.swift
+++ GroupProject/New Folder 2/JournalEntryViewController.swift
@@ -1,29 +1,36 @@
-//
 //  JournalEntryViewController.swift
 //  GroupProject
-//
 //  Created by Mondrelle Cheek on 4/19/26.
-//
-
 import UIKit
 
+//Class
 class JournalEntryViewController: UIViewController {
-
+    
+    //Outlets
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var subjectLabel: UITextField!
+    @IBOutlet weak var entryField: UITextView!
+    
+    //We need to be able to save entries for future
+    struct JournalEntry {
+        //Variables
+        var date: String?
+        var subject: String?
+        var entry: String?
+    }//End of struct
+    var journalEntries: [JournalEntry] = []
+    
+    
+    
+    
+    
+    
+    
+    
+    //Overrride
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
--- GroupProject/New Folder/MoodTrackerViewController.swift
+++ GroupProject/New Folder/MoodTrackerViewController.swift
@@ -1,29 +1,63 @@
-//
 //  MoodTrackerViewController.swift
 //  GroupProjectProposal
-//
 //  Created by Mondrelle Cheek on 4/16/26.
-//
-
 import UIKit
 
+//Class
 class MoodTrackerViewController: UIViewController {
-
+    
+    //Variables
+    var mood: String?//"how are you feeling today"
+    var moodSubject: String? //"what contributed to your mood todya?"
+    var energy: Int? //using a slider 1-10
+    
+    //taps (moods)
+    @IBAction func happyTapped(_ sender: Any) {
+        mood = "Happy"
+        moodLabel.text = "I am happy"
+    }
+    @IBAction func okayTapped(_ sender: Any) {
+        mood = "Okay"
+        moodLabel.text = "I am okay"
+    }
+    @IBAction func sadTapped(_ sender: Any) {
+        mood = "Sad"
+        moodLabel.text = "Im sad"
+    }
+    @IBAction func madTapped(_ sender: Any) {
+        mood = "Mad"
+        moodLabel.text = "Im mad"
+    }
+    @IBAction func disTapped(_ sender: Any) {
+        mood = "Dissapointed"
+        moodLabel.text = "Im dissapointed"
+    }
+    @IBAction func boredTapped(_ sender: Any) {
+        mood = "Bored"
+        moodLabel.text = "Im bored"
+    }
+    @IBAction func fatTapped(_ sender: Any) {
+        mood = "Fatigued"
+        moodLabel.text = "Im fatigued"
+    }
+    @IBAction func tiredTapped(_ sender: Any) {
+        mood = "Tired"
+        moodLabel.text = "Im tired"
+    }
+    //moodLabel that gets changed when emoji is tapped
+    @IBOutlet weak var moodLabel: UILabel!
+    
+    
+    
+    
+    @IBAction func moodLogTapped(_ sender: Any) {
+    }
+    
+    
+    //ViewDidLoad
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
-}
+}//End of the MoodTracker ViewController
