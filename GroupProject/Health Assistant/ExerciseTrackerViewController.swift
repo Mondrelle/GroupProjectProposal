1ebfe3c8053aa133ff8ce8fd5e10ce313205c2e7
Mondrelle <mario_cheek10@mymail.eku.edu>
2026-04-21 03:44:47 -0400

    Calorie Tracker, Wellness, and Exercise Tracker Controllers developed

--- GroupProject/Health Assistant/ExerciseTrackerViewController.swift
+++ GroupProject/Health Assistant/ExerciseTrackerViewController.swift
@@ -1,36 +1,63 @@
-//  calorieTrackerViewController.swift
-//  GroupProject
-//
 //  Created by Mondrelle Cheek on 4/20/26.
-//
-
 import UIKit
-
 class ExerciseTrackerViewController: UIViewController {
-
-    
     @IBOutlet weak var userSteps: UITextField!
     @IBOutlet weak var workoutDuration: UITextField!
-    
-    
     @IBAction func feedbackTapped(_ sender: Any) {
-        //check if they filled in fields
+        //Check if they filled in all fields
+        if userSteps.text == "" || workoutDuration.text == "" {
+            let alert = UIAlertController(
+                title: "Missing Information",
+                message: "Please fill in all fields",
+                preferredStyle: .alert
+            )
+            alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+            present(alert, animated: true, completion: nil)
+            return
+        }
+        
+        //Check if they typed in numbers
+        guard let steps = Int(userSteps.text ?? ""),
+              let minutes = Int(workoutDuration.text ?? "") else {
+            let alert = UIAlertController(
+                title: "Invalid Input",
+                message: "Please enter numbers only.",
+                preferredStyle: .alert
+            )
+            alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+            present(alert, animated: true, completion: nil)
+            return
+        }
+        
+        var results = ""
+        
+        //steps
+        if steps < 10000{
+            let stepsLeft = 10000 - steps
+            results += "You have \(stepsLeft) steps left to reach your goal!\n"
+        }else{
+            results += "You have reached your goal!\n"
+        }
+            
+        ///workout
+        let estimatedActiveMinutes = 30 - (steps / 100)
+        results += "Note: From walking alone, you've exercised for approximately \(estimatedActiveMinutes) minutes.\n"
+        let minsLeft = 30 - minutes
+        if minutes < 30{
+            results += "You have \(minsLeft + estimatedActiveMinutes) minutes left to reach your 30 minute goal!\n"
+        } else {
+            results += "You have reached your 30 minute goal!\n"
+        }
+        
+        feedback.text = results
         //10,000 steps goal, 1,000 steps is 10 minutes
         //At least 30 minute workout, and this is how close you are
     }
-    
-    
     @IBOutlet weak var feedback: UITextView!
-    
-    
-    
-    
-    
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
-    
 }
--- GroupProject/Health Assistant/WellnessViewController.swift
+++ GroupProject/Health Assistant/WellnessViewController.swift
@@ -8,33 +8,30 @@
 import UIKit
 
 class WellnessViewController: UIViewController {
-
-    
     @IBOutlet weak var userSleep: UITextField!
     @IBOutlet weak var userWater: UITextField!
     @IBOutlet weak var userPhone: UITextField!
     @IBOutlet weak var userStress: UITextField!
     @IBOutlet weak var feedback: UITextView!
-    
-    
     @IBAction func feedbackTapped(_ sender: Any) {
         //Did user enter values?
+        if userSleep.text == "" || userWater.text == "" || userPhone.text == "" || userStress.text == "" {
+            let alert = UIAlertController(
+            title: "Missing Information",
+            message: "Please fill in all fields",
+            preferredStyle: .alert
+            )
+            alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+            present(alert, animated: true)
+            return
+        }
         //user needs to sleep more 8 hrs a day, needs more water 2.5L a day, and cant exceed four hours of phone time
         
     }
-    
-    
-    
-    
-    
-    
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
-    
-
-
 }
