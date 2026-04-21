de856c0015a53ddfcf4bc61983400d22410f3395
Mondrelle <mario_cheek10@mymail.eku.edu>
2026-04-21 02:14:59 -0400

    The code for my calorie tracker view controller has been finished. Enter your cals, proteins, sugars in grams and the tracker will respond with feedback

--- GroupProject/Health Assistant/CalorieTrackerViewController.swift
+++ GroupProject/Health Assistant/CalorieTrackerViewController.swift
@@ -8,25 +8,76 @@
 import UIKit
 
 class CalorieTrackerViewController: UIViewController {
-
+    
     @IBOutlet weak var userCals: UITextField!
     @IBOutlet weak var userProtein: UITextField!
     @IBOutlet weak var userSugars: UITextField!
+    @IBOutlet weak var feedback: UITextView!
     
     
     
     
     @IBAction func feedbackTapped(_ sender: Any) {
-        //check if users even entered anything, and if the vaules work
-        //men need 2000, women 1800, at least 50, under 36 grams of sugar?
-        //you need to eat more/less cals/protein, youve went over sugars
+        //Check if user even entered anything, and if the vaules work
+        //So we check if the text fields are empty
+        if userCals.text == "" || userProtein.text == "" || userSugars.text == "" {
+            let alert = UIAlertController(
+                title: "Missing Information",
+                message: "Please enter calories, proteins, and sugars",
+                preferredStyle: .alert
+            )
+            alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+            present(alert, animated: true, completion: nil)
+            return
+        }//End of if{} - Did the user fill all fields
         
+        //We take the values from the field and create new variables
+        guard let calories = Int(userCals.text!),
+              let protein = Int(userProtein.text!),
+              let sugars = Int(userSugars.text!) else {
+            let alert = UIAlertController(
+                title: "Invalid Input",
+                message: "Please enter numbers only (no symbols or letters)",
+                preferredStyle: .alert
+            )//end of let aleert
+            alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+            present(alert, animated: true, completion: nil)
+            return
+        }//end of else
+        
+        //Criteria: At least 1800, but no more than 2200 calories. At least 50 protein. Less than 36 grams of sugar?
+        //feedback
+        var feedbackText = ""
+        
+        //Calories
+        if calories < 1800 {
+            feedbackText += "Your calories are low. You may need more food today "
+        }else if calories <= 2200 {
+            feedbackText += "Your calorie amount looks in a healthy average range "
+        }else {
+            feedbackText += "Your calories are high. "
+        }//end of calorie
+        
+        //Proteins
+        if protein < 50 {
+            feedbackText += "Your protein is low. You may need more food today "
+        } else {
+            feedbackText += "You've it your protein goals for today "
+        }//end of protein
+        
+        //Sugars
+        if sugars > 36 {
+            feedbackText += "You've gone over your sugar intake for today. Please consider limiting your intake of sugary foods "
+        } else {
+            feedbackText += "You've stayed within your sugar intake for today "
+        }
+        
+        feedback.text = feedbackText
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
         // Do any additional setup after loading the view.
+        
     }
-    
 }
